dfbfafb4d8df927fe3e896275978258624fbd63e
fox <fox@moneyball.io>
2016-04-10 18:45:43 +0800

    Add article order use published_at

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -12,7 +12,7 @@ class ArticlesController extends Controller
 {
     public function index()
     {
-        $articles = Article::all();
+        $articles = Article::latest('published_at')->get();
 
         return view('articles.index', compact('articles'));
     }
