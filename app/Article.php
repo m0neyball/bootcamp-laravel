b8a9f0f77b392a15082025de514bda73ceea76b2
fox <fox@moneyball.io>
2016-04-10 15:27:14 +0800

    Add article model ide helper and fillable column

--- app/Article.php
+++ app/Article.php
@@ -4,7 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Article
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $body
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property string $published_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereTitle($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereBody($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article wherePublishedAt($value)
+ * @mixin \Eloquent
+ */
 class Article extends Model
 {
-    //
+    protected $fillable = ['title', 'body', 'published_at'];
 }
