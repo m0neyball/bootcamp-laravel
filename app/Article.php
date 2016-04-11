e160755751b3eab2c871aa9b6e941e90541a24b7
fox <fox@moneyball.io>
2016-04-11 18:02:16 +0800

    Add set published at attribute

--- app/Article.php
+++ app/Article.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -24,4 +25,14 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $fillable = ['title', 'body', 'published_at'];
+
+    /**
+     * setNameAttribute
+     *
+     * @param $data
+     */
+    public function setPublishedAtAttribute($data)
+    {
+        $this->attributes['published_at'] = Carbon::createFromFormat('Y-m-d', $data);
+    }
 }
