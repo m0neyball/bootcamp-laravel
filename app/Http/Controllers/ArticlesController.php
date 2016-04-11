2b95cf61777970161afae0e162f316706121dfdd
fox <fox@moneyball.io>
2016-04-11 18:12:04 +0800

    Add article show filter published at let now

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -12,7 +12,7 @@ class ArticlesController extends Controller
 {
     public function index()
     {
-        $articles = Article::latest('published_at')->get();
+        $articles = Article::latest('published_at')->where('published_at', '<=', Carbon::now())->get();
 
         return view('articles.index', compact('articles'));
     }
