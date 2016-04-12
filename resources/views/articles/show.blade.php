b138129093a1ebb3c003cfab3420c807d02c5ef8
lyndon <lyndon@moneyball.io>
2016-04-12 16:37:36 +0800

    Add ep9 ArticlesController and articles view

---
+++ resources/views/articles/show.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+
+@section('content')
+    <article>
+        <h2>
+            {{$article->title}}
+        </h2>
+        <div class="body">{{$article->body}}</div>
+    </article>
+@stop
