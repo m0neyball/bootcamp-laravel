bfd9ce3e26af44ecfa68acfe38c3becaa75f76d4
lyndon <lyndon@moneyball.io>
2016-04-13 02:51:18 +0800

    Fix create view

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -6,7 +6,19 @@
     <hr/>
 
     {!! Form::open() !!}
+    <div class="form-group">
+        {!! Form::label('title','Title:') !!}
+        {!! Form::text('title', null, ['class' => 'form-control']) !!}
+    </div>
 
-
+    <!-- Body Form Input -->
+    <div class="form-group">
+        {!!  Form::label('body', 'Body') !!}
+        {!!  Form::textarea('body', null, ['class' => 'form-control']) !!}
+    </div>
+    <!-- Add Article From Input-->
+    <div class="form-group">
+        {!! Form::submit('Add Article',['class' => 'btn btn-primary form-control']) !!}
+    </div>
     {!! Form::close() !!}
 @stop
