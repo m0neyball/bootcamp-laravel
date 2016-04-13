a4935a996b86d730f084a6110ea417d217885dc4
ben <ben@moneyball.io>
2016-04-13 15:29:27 +0800

    Fix all Authentication Resetting Passwords

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,8 +32,15 @@ Route::get('articles/{id}/edit', 'ArticlesController@edit');
 
 Route::resource('articles', 'ArticlesController');
 
-Route::group(['namespace' => 'Auth'], function()
-{
+Route::group(['namespace' => 'Auth'], function () {
     Route::Controller('auth', 'AuthController');
-    Route::Controller('password', 'PasswordController');
+
+    // Route::Controller('password', 'PasswordController');
+    // 密碼重置連結的路由...
+    Route::get('password/email', 'PasswordController@getEmail');
+    Route::post('password/email', 'PasswordController@postEmail');
+
+    // 密碼重置的路由...
+    Route::get('password/reset/{token}', 'PasswordController@getReset');
+    Route::post('password/reset', 'PasswordController@postReset');
 });
