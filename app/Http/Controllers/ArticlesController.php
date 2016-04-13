d01d4382fd3debcb93fd70ef09d8432ab75e6c8e
lyndon <lyndon@moneyball.io>
2016-04-13 11:15:32 +0800

    Add dates in Article Model and test this feature

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -24,7 +24,8 @@ class ArticlesController extends Controller
     public function show($id)
     {
         $article = Article::findOrFail($id);
-
+//        dd($article->updated_at->addDays(8)->diffForHumans());
+        dd($article->published_at);
         return view('articles.show',compact('article'));
     }
 
