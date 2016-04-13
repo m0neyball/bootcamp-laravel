fec782fcaeadfcbf5a8a6d001598a95d168d4b07
ben <ben@moneyball.io>
2016-04-13 09:39:45 +0800

    Add basic login page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,33 @@
+@extends('app')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <h2>Login</h2>
+
+            <form action="/auth/login" method="POST">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                <p>
+                    <label for="email">Email:</label><br>
+                    <input id="email" name="email" type="email" value="{{ old('email') }}">
+                    @if ($errors->has('email'))
+                        <strong>{{ $errors->first('email') }}</strong>
+                    @endif
+                </p>
+
+                <p>
+                    <label for="password">Password:</label><br>
+                    <input id="password" name="password" type="password" value="{{ old('password') }}">
+                    @if ($errors->has('password'))
+                        <strong>{{ $errors->first('password') }}</strong>
+                    @endif
+                </p>
+
+                <p>
+                    <input type="submit" value="Login">
+                </p>
+            </form>
+        </div>
+    </div>
+@endsection
