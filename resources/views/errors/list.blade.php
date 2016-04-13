4ebec87c1bdf9949e7203a93d3da8dec6acde75e
lyndon <lyndon@moneyball.io>
2016-04-13 16:05:06 +0800

    Add error list view

--- resources/views/errors/list.blade.php
+++ resources/views/errors/list.blade.php
@@ -1,7 +1,7 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: mac
- * Date: 2016/4/13
- * Time: 下午3:59
- */
+@if($errors->any())
+    <ul class="alert alert-danger">
+        @foreach($errors->all() as $error)
+            <li>{{$error}}</li>
+        @endforeach
+    </ul>
+@endif
