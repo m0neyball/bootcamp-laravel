606614cc09a41488269d0ea5cc784a1f749f320a
lyndon <lyndon@moneyball.io>
2016-04-13 12:36:48 +0800

    Fix store in ArticlesController

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,24 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use App\Http\Requests\CreateArticleRequest;
 use Carbon\Carbon;
-use Request;
+//use Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class ArticlesController extends Controller
 {
-    //
+
+    /**
+     * article index
+     *
+     * @return mixed
+     */
     public function index()
     {
 //        $articles = Article::latest('published_at')->get();
 //        $articles = Article::latest('published_at')->where('published_at','<=', Carbon::now())->get();
-        $articles = Article::latest('published_at')->unpublished()->get();
+        $articles = Article::latest('published_at')->published()->get();
         
         return view('articles.index',compact('articles'));
     }
 
+    /**
+     * show article
+     *
+     * @param $id
+     *
+     * @return mixed
+     */
     public function show($id)
     {
         $article = Article::findOrFail($id);
@@ -29,15 +42,28 @@ class ArticlesController extends Controller
         return view('articles.show',compact('article'));
     }
 
+
+    /**
+     * create new article.
+     *
+     * @return mixed
+     */
     public function create()
     {
         return view('articles.create');
     }
 
-    public function store()
+    /**
+     * save new article.
+     *
+     * @param CreateArticleRequest $request
+     *
+     * @return mixed
+     */
+    public function store(CreateArticleRequest $request)
     {
 
-        Article::create(Request::all());
+        Article::create($request->all());
 
         return redirect('articles');
     }
