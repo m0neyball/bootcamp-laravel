2f01a00f3e66b487c530deb76326a9a4c4d12664
lyndon <lyndon@moneyball.io>
2016-04-13 16:10:46 +0800

    Fix create view and edit view's error msg block

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -29,11 +29,5 @@
     </div>
     {!! Form::close() !!}
 
-    @if($errors->any())
-        <ul class="alert alert-danger">
-            @foreach($errors->all() as $error)
-                <li>{{$error}}</li>
-            @endforeach
-        </ul>
-    @endif
+    @include('errors.list')
 @stop
