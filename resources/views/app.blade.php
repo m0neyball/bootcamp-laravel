75da69c403e07ba59b87ad4968e51fbd44ae6d40
ben <ben@moneyball.io>
2016-04-14 23:26:38 +0800

    Fix app.blade use partials.flash ( video 20 )

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,17 +9,7 @@
 </head>
 <body>
 <div class="container">
-    @if(Session::has('flash_message'))
-        <div class="alert alert-success {{ Session::has('flash_message_important') ? 'alert-important' : '' }}" role="alert">
-            @if(Session::has('flash_message_important'))
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            @endif
-
-            {{ session('flash_message') }}
-        </div>
-    @endif
+    @include('partials.flash')
 
     @yield('content')
 </div>
