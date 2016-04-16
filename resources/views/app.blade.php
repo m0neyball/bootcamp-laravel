39e5468e98acf4f692cd155835543b0aa1560112
ben <ben@moneyball.io>
2016-04-16 10:45:03 +0800

    Add app.blade and form.blade use select2 jquery plugin ( video 24 )

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
           integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/css/select2.min.css" rel="stylesheet"/>
     <link rel="stylesheet" href="{{ elixir('output/final.css') }}">
 </head>
 <body>
@@ -20,6 +21,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
         integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
         crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/js/select2.min.js"></script>
 
 @if(!Session::get('flash_notification.important'))
     <script>
@@ -31,5 +33,7 @@
     <script>
         $('#flash-overlay-modal').modal();
     </script>
+
+    @yield('script')
 </body>
 </html>
