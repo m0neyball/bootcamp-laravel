3a8fdb5652f962714e6649a9288da2bb9be7a205
lyndon <lyndon@moneyball.io>
2016-04-19 10:00:35 +0800

    Add selecet2 in form view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{elixir('css/all.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/css/select2.min.css"/>
 </head>
 <body>
     <div class="container">
@@ -14,6 +15,7 @@
         @yield('content')
     </div>
     <script src="//code.jquery.com/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/js/select2.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <script>
         $('#flash-overlay-modal').modal();
