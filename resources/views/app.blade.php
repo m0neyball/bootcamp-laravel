8f55f1be9f41f269efef248947c8a4c24e0d4083
lyndon <lyndon@moneyball.io>
2016-04-19 11:59:51 +0800

    Add nav view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,13 +9,17 @@
 
 </head>
 <body>
+    <script src="{{elixir('js/all.js')}}"></script>
+
+    @include('partials.nav')
+
     <div class="container">
         @include('flash::message')
 
         @yield('content')
     </div>
-    <script src="{{elixir('js/all.js')}}"></script>
-   
+
+
     <script>
         $('#flash-overlay-modal').modal();
 //        $('div.alert').not('.alert-important').delay(3000).slideUp(300);
