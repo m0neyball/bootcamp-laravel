8f2481bec7dcd2038333b831c49d5f49e3085184
jilingchiu <jilign@degather.com>
2016-08-11 12:04:02 +0800

    Add product list layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,5 +9,48 @@
     </head>
     <body>
 
+    <div id="container">
+
+        <div id="header">
+            <div class="hot"><a href="#">home</a> | <a href="#">sitemap</a> | <a href="#">contact</a></div>
+            <div class="logo">LOGO</div>
+            <div class="nav">
+                <ul>
+                    <li><a href="#">編輯頁</a></li>
+                    <li><a href="#">訊息列表</a></li>
+                    <li><a href="#">產品展示</a></li>
+                    <li><a href="#">表單</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div id="content">
+            <div id="aside">
+                <div class="nav">
+                    <ul>
+                        <li><a href="#">圖文配置</a></li>
+                        <li><a href="#">menu2</a></li>
+                        <li><a href="#">menu3</a></li>
+                        <li><a href="#">menu4</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div id="article">
+                <div class="hgroup">
+                    <h1>編輯頁</h1>
+                    <div class="way"><a href="#">home</a> &gt; <a href="#">pagename</a></div>
+                </div>
+                <div class="section">
+                    <h2>文章標題</h2>
+                    <p>文章段落</p>
+                </div>
+            </div>
+        </div>
+
+        <div id="footer"> <p> 2016 &copy; Design by Jiling </p></div>
+
+    </div>
+
     </body>
 </html>
